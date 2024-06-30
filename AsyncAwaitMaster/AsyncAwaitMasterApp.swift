6a9a9b506e12c54a9fbc2f6811838e9e3a78b46c
Jorge Flores Carlos <florescarlos167@gmail.com>
2024-06-30 10:59:21 -0600

    Initial Commit

---
+++ AsyncAwaitMaster/AsyncAwaitMasterApp.swift
@@ -0,0 +1,32 @@
+//
+//  AsyncAwaitMasterApp.swift
+//  AsyncAwaitMaster
+//
+//  Created by Jorge Flores Carlos on 30/06/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AsyncAwaitMasterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
